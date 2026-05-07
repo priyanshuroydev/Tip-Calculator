c769d322d8d88f7f284d12c983a07633921c9e21
priyanshuroydev <priyanshuroy543@gmail.com>
2026-05-07 22:25:41 +0530

    Add options and transitions

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let box = document.querySelector(".box")
+let box = document.querySelector(".box");
+let yesButton = document.querySelector("#yes");
+let noButton = document.querySelector("#no");
+let clickHere = document.querySelector(".clickHere");
+let options = document.querySelector(".options");
 
+const openTipCalculator = () => {
+    box.style.height = "600px";
+    clickHere.style.display = "none";
+    yesButton.style.display = "none";
+    noButton.style.display = "none";
+};
+
+const tryAgain = () => {
+    clickHere.innerText = "OK , You Want To Try Again ?";
+    yesButton.style.display = "none";
+};
+
+yesButton.addEventListener("click", openTipCalculator);
+noButton.addEventListener("click", tryAgain);
