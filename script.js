8f4f3f4ae73511f7837825eb22780668bf78886f
priyanshuroydev <priyanshuroy543@gmail.com>
2026-05-12 13:42:14 +0530

    debug bugs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let yesButton = document.querySelector("#yes");
 let noButton = document.querySelector("#no");
 let clickHere = document.querySelector(".clickHere");
 let options = document.querySelector(".options");
-let number = document.querySelector(".number")
+let number = document.querySelectorAll(".number")
 let display = document.querySelector(".display")
 let amount = document.querySelector(".amount")
 let tip = document.querySelector(".tip")
@@ -14,8 +14,8 @@ let saveAmount = 0
 let saveTip = 0
 let saveTotal = 0 
 
-const numberType = ()=>{
-  saveAmount = number.textContent
+const numberType = (button)=>{
+  saveAmount = button.textContent
   amount.append(saveAmount)
   
 }
@@ -34,4 +34,6 @@ yesButton.addEventListener("click", openTipCalculator);
 noButton.addEventListener("click", tryAgain);
 
 
-number.addEventListener("click",numberType)
+number.forEach((button)=>{
+  number.addEventListener("click",numberType)
+})
