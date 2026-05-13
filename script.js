b9d4cbc5d5217823dbc7bd1ffced073423d6cc42
priyanshuroydev <priyanshuroy543@gmail.com>
2026-05-13 11:34:49 +0530

    rework

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let noButton = document.querySelector("#no");
 let clickHere = document.querySelector(".clickHere");
 let options = document.querySelector(".options");
 let number = document.querySelectorAll(".number")
-let display = document.querySelector(".display")
+let tipDisplay = document.querySelector(".tipDisplay")
 let amount = document.querySelector(".amount")
 let tip = document.querySelector(".tip")
 let total = document.querySelector(".total")
@@ -13,27 +13,3 @@ let total = document.querySelector(".total")
 let saveAmount = 0
 let saveTip = 0
 let saveTotal = 0 
-
-const numberType = (button)=>{
-  saveAmount = button.textContent
-  amount.append(saveAmount)
-  
-}
-const openTipCalculator = () => {
-  box.style.height = "600px";
-  clickHere.style.display = "none";
-  yesButton.style.display = "none";
-  noButton.style.display = "none";
-};
-
-const tryAgain = () => {
-  clickHere.innerText = "OK , You Want To Try Again ?";
-};
-
-yesButton.addEventListener("click", openTipCalculator);
-noButton.addEventListener("click", tryAgain);
-
-
-number.forEach((button)=>{
-  number.addEventListener("click",numberType)
-})
